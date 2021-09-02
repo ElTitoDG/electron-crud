e53ad7994e26fc20b8a63b0d65167bd6f9e1423e
Julian <julianhinojosagil@gmail.com>
2021-09-02 14:16:50 +0200

    Basic Interface 1

--- src/main.js
+++ src/main.js
@@ -1,4 +1,30 @@
-const {BrowserWindow} = require('electron')
+const {BrowserWindow, Notification} = require('electron')
+const {getConnection} = require('./database')
+
+async function createProduct(product) {
+    try {
+        const conn = await getConnection();
+        product.price = parseFloat(product.price)
+        const result = await conn.query('INSERT INTO product SET ?', product)
+
+        new Notification({
+            title: 'ElectronDB',
+            body: 'Nuevo Producto guardado'
+        }).show();
+
+        product.id = result.insertId;
+        return product;
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+async function getProducts() {
+    const conn = await getConnection();
+    const results = await conn.query('SELECT * FROM product ORDER BY id DESC')
+    console.log(results)
+    return results;
+}
 
 let window
 
@@ -7,12 +33,16 @@ function createWindow() {
         width: 1920,
         height: 1080,
         webPreferences: {
-            nodeIntegration: true
-        }
+            nodeIntegration: true,
+            contextIsolation: false,
+            enableRemoteModule: true,
+        },
     })
     window.loadFile('src/ui/index.html');
 }
 
 module.exports = {
-    createWindow
+    createWindow,
+    createProduct,
+    getProducts
 }
